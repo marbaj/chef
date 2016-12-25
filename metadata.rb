493d2656eaad257e2391446a0f8428a1a1d9f8f3
Marko Bajic <Marko@hellowallet.com>
2016-12-25 13:32:08 -0500

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name        "opsworks_nodejs"
+description 'Installs and configures a Node.js application server'
+maintainer  "Hellowalet Marko Bajic"
+version     "0.0.1"
+
+depends 'deploy'
+depends 'opsworks_commons'
