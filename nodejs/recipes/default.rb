bd34250bbc07cd9fd661edf71ea536cf0810012e
Marko Bajic <Marko@hellowallet.com>
2016-12-27 14:47:06 -0500

    new files

--- nodejs/recipes/default.rb
+++ nodejs/recipes/default.rb
@@ -1,11 +1,12 @@
 execute "update-upgrade" do
-  command "apt-get update && apt-get upgrade -y"
-  action :run
+    command "apt-get update && apt-get upgrade -y"
+    action :run
 end
 
-file '/var/www/customers/public_html/index.php' do
-  content '<html>This is a placeholder for the home page.</html>'
-  mode '0755'
-  owner 'web_admin'
-  group 'web_admin'
+file '/var/text.txt' do
+    content '<html>This is a placeholder for the home page.</html>'
+    mode '0755'
+    owner 'root'
+    group 'root'
+    action :create
 end
---
+++ nodejs/recipes/test.rb
@@ -0,0 +1,6 @@
+file '/var/_text.txt' do
+    owner "root"  
+    group "root"  
+    mode "0755"  
+    action :create
+end
